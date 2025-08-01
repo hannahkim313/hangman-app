24b0ae59bf1b1affbb9855d9cd9a3406b3a179c9
ashp8 <a.phillips7@outlook.com>
2025-07-31 18:37:26 -0700

    Improve code semantics

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex min-h-screen flex-col items-center justify-start space-y-5 px-5 pt-[30vh] text-center sm:px-0 sm:pt-[40vh]">
       <h1 className="text-7xl font-bold">404</h1>
       <p className="text-2xl">Invalid guess! This page could not be found.</p>
-      <Link href={'/'}>Go back home</Link>
+      <Link href="/">Go back home</Link>
     </div>
   );
 }
